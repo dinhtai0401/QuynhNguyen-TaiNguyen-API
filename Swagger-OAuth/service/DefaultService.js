965bdba464c799451a7499123989f1422af38ab1
Tai Nguyen <tainguyen@Tais-MacBook-Pro.local>
2020-02-26 01:18:28 +0200

    new

---
+++ Swagger-OAuth/service/DefaultService.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+/**
+ * Server heartbeat operation
+ * This operation shows how to override the global security defined above, as we want to open it up for all users.
+ *
+ * no response value expected for this operation
+ **/
+exports.usersGET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
